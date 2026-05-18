091ffdc2f80c31ea4ce7bb6365c923314762d323
bachratu <pawelbienek@gmail.com>
2026-05-18 16:57:06 +0200

    first commit

---
+++ src/main/java/com/bachratus/expenses_monitor/ExpensesMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.bachratus.expenses_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensesMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bachratus/expenses_monitor/ExpensesMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bachratus.expenses_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
